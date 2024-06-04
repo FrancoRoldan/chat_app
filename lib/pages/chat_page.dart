f6cec189e51a5cce4240311608a1b868dad8f361
franco <francosantiago22@gmail.com>
2024-06-03 22:19:52 -0300

    inicio chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,9 +5,43 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('chat page'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: const Column(
+          children: [
+            CircleAvatar(
+              maxRadius: 14,
+              child: Text(
+                'Fr',
+                style: TextStyle(fontSize: 12, color: Colors.indigoAccent),
+              ),
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Text(
+              'Franco Roldán',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+              child: ListView.builder(
+                  reverse: true, itemBuilder: (_, i) => Text(i.toString()))),
+          const Divider(
+            height: 1,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+          )
+        ],
       ),
     );
   }
