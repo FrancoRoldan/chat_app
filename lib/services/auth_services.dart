8ab730066b2a71c35b84014f2ee161923eb9dc5e
franco <francosantiago22@gmail.com>
2024-06-04 20:03:02 -0300

    mapear la respuesta del login

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,16 +1,23 @@
 import 'dart:convert';
 
 import 'package:chat_app/global/enviroment.dart';
+import 'package:chat_app/models/login_response.dart';
+import 'package:chat_app/models/usuarios.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AuthServices extends ChangeNotifier {
+  Usuario? usuario;
+
   Future login(String email, String password) async {
     final data = {'email': email, 'password': password};
 
     final resp = await http.post(Uri.parse('${Enviroment.apiUrl}/login'),
         body: jsonEncode(data), headers: {'Content-Type': 'application/json'});
 
-    print(resp.body);
+    if (resp.statusCode == 200) {
+      final loginResponse = loginResponseFromJson(resp.body);
+      usuario = loginResponse.usuario;
+    }
   }
 }
