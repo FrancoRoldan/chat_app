805ac428c8823a43008b5b83a264814edd212ab3
franco <francosantiago22@gmail.com>
2024-06-03 21:16:44 -0300

    pull to refresh

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/models/usuarios.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class UsuariosPage extends StatefulWidget {
   const UsuariosPage({super.key});
@@ -9,6 +10,9 @@ class UsuariosPage extends StatefulWidget {
 }
 
 class _UsuariosPageState extends State<UsuariosPage> {
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
   final usuarios = [
     Usuario(uid: '1', nombre: 'María', email: 'test1@test.com', online: true),
     Usuario(
@@ -38,7 +42,16 @@ class _UsuariosPageState extends State<UsuariosPage> {
           ],
         ),
         body: SafeArea(
-          child: _listViewUsuarios(),
+          child: SmartRefresher(
+            controller: _refreshController,
+            enablePullDown: true,
+            onRefresh: _cargarUsuarios,
+            header: WaterDropHeader(
+              complete: Icon(Icons.check, color: Colors.blue[400]),
+              waterDropColor: Colors.blue,
+            ),
+            child: _listViewUsuarios(),
+          ),
         ));
   }
 
@@ -66,5 +79,11 @@ class _UsuariosPageState extends State<UsuariosPage> {
             borderRadius: BorderRadius.circular(100)),
       ),
     );
+  }
+
+  _cargarUsuarios() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
