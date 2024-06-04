d5f6746dde25c15b6a4dba4aee6dd792351796a3
franco <francosantiago22@gmail.com>
2024-06-03 21:04:21 -0300

    inicio de usuarios page

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      initialRoute: 'login',
+      initialRoute: 'usuarios',
       routes: appRoutes,
     );
   }
--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,13 +1,69 @@
+import 'package:chat_app/models/usuarios.dart';
 import 'package:flutter/material.dart';
 
-class UsuariosPage extends StatelessWidget {
+class UsuariosPage extends StatefulWidget {
   const UsuariosPage({super.key});
 
   @override
+  State<UsuariosPage> createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+  final usuarios = [
+    Usuario(uid: '1', nombre: 'María', email: 'test1@test.com', online: true),
+    Usuario(
+        uid: '2', nombre: 'Melissa', email: 'test2@test.com', online: false),
+    Usuario(
+        uid: '3', nombre: 'Fernando', email: 'test3@test.com', online: true),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('usuarios page'),
+    return Scaffold(
+        appBar: AppBar(
+          title:
+              const Text('Mi Nombre', style: TextStyle(color: Colors.black87)),
+          elevation: 1,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.exit_to_app, color: Colors.black87),
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: Icon(Icons.check_circle, color: Colors.blue[400]),
+              // child: Icon( Icons.offline_bolt, color: Colors.red ),
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: _listViewUsuarios(),
+        ));
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (_, i) => _usuarioListTile(usuarios[i]),
+        separatorBuilder: (_, i) => const Divider(),
+        itemCount: usuarios.length);
+  }
+
+  ListTile _usuarioListTile(Usuario usuario) {
+    return ListTile(
+      title: Text(usuario.nombre!),
+      subtitle: Text(usuario.email!),
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[100],
+        child: Text(usuario.nombre!.substring(0, 2)),
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            color: usuario.online! ? Colors.green[300] : Colors.red,
+            borderRadius: BorderRadius.circular(100)),
       ),
     );
   }
